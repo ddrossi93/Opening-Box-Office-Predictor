0bddb422e1129a7eb199e0a7dac54201c52ae949
Dylan Rossi <ddrossi93@gmail.com>
2018-07-04 03:03:55 -0500

    Started getting data ready for regression

--- linear_regression.py
+++ linear_regression.py
@@ -21,25 +21,25 @@ def backwardElimination(x, sl):
     return x
 
 # Importing the dataset
-dataset = pd.read_csv('cleaned_data.csv')
+dataset = pd.read_csv('actor_genre.csv')
 
 
 
 
-X = dataset.iloc[:, 2:].values
-y = dataset.iloc[:, 1].values
+X = dataset.iloc[:, 1:].values
+y = dataset.iloc[:, 0].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 
 # Feature Scaling
-# from sklearn.preprocessing import StandardScaler
-# sc_X = StandardScaler()
-# X_train = sc_X.fit_transform(X_train)
-# X_test = sc_X.transform(X_test)
-# sc_y = StandardScaler()
-# y_train = sc_y.fit_transform(y_train)
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train)
 
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -48,14 +48,16 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+regressor.score(X_test, y_test)
 
 # test significance of variables
 import statsmodels.formula.api as sm
-X = np.append(arr = np.ones((186, 1)).astype(int), values = X, axis = 1)
+X = np.append(arr = np.ones((175, 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, :]
 SL = 0.1
 
 X_Modeled = backwardElimination(X_opt, SL)
+X_Modeled = X_Modeled[:, 1:]
 
 # refit model using optimal x variables
 from sklearn.cross_validation import train_test_split
